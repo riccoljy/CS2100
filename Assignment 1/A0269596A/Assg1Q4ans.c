aea212ccd20978561e6f0082c0773038a210de8e
Ricco Lim <limricco2002@gmail.com>
2024-09-15 11:19:53 +0800

    Change to return int

--- Assignment 1/A0269596A/Assg1Q4ans.c
+++ Assignment 1/A0269596A/Assg1Q4ans.c
@@ -1,9 +1,10 @@
-void func(int a) {
+int func(int a) {
     int i = 32, count = 0;
     while (i > 0) {
         if (a >= 0) count++;
         i--;
-        if (i == 0) return;
+        if (i == 0) break;
         a *= 2;
     }
+    return count;
 }
