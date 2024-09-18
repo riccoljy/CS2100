4a390285d32fd30d595f341d283f79d9dcaa9c2e
Ricco Lim <limricco2002@gmail.com>
2024-09-18 19:01:04 +0800

    + Lab 3

--- Assignment 1/A0269596A/Assg1Q4ans.c
+++ Assignment 1/A0269596A/Assg1Q4ans.c
@@ -1,3 +1,17 @@
+// CS2100 Computer Organisation
+// AY2024/2025 Semester 1
+// National University of Singapore.
+// Assignment 1 Question 4.
+// All rights reserved.
+//
+// Start by filling in the following variables which main() will print out so as to identify yourself.
+// Failure to full in the details correctly will be penalised.
+//
+
+const char *student_number = "A0269596A";
+const char *student_name = "Ricco Lim";
+const char *student_tut_grp = "T07";
+
 int func(int a) {
     int i = 32, count = 0;
     while (i > 0) {
